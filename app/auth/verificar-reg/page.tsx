fe37f437755406a887a8a8c9e45825ebffa9fa5a
Jhonny Toro <toro.jhonny@gmail.com>
2025-06-06 16:16:27 -0500

    elimine llamar veri reg

--- app/auth/verificar-reg/page.tsx
+++
@@ -1,9 +0,0 @@
-import { VerificationForm } from "@/components/auth/verification-form"
-
-export default function VerificationPage() {
-  return (
-    <div className="container mx-auto px-4 py-12">
-      <VerificationForm />
-    </div>
-  )
-}
